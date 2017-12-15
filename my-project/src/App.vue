4603493f028ec9648f1881dddc8078f171206761
zhanghaifeng <429654754@qq.com>
2017-12-15 21:17:27 +0800

    project

---
+++ my-project/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+/*初始化*/
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: "Microsoft YaHei",Arial, Helvetica, sans-serif;
+    color: #333;
+  }
+
+  body{
+    min-width: 980px;
+  }
+
+  ul,ol {
+    list-style: none;
+  }
+
+  a,a:hover,a:active,a:visited {
+    text-decoration: none;
+    color: #333;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  img {
+    border: none;
+  }
+
+  button,input,select,textarea {
+    font-size: 100%;
+  }
+</style>
---
+++ my-project/src/components/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="index">
+
+    <!--侧边背景图-->
+    <section class="back_left"></section>
+    <section class="back_right"></section>
+
+    <!--导航-->
+    <nav>
+      <a href="javascript:;"></a>
+      <a href="javascript:;"></a>
+      <a href="javascript:;"></a>
+      <a href="javascript:;"></a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #index {
+    width: 100%;
+    height: 100vh;
+    background: url("../assets/index-back.jpg") no-repeat;
+    background-size: cover;
+    position: relative;
+  }
+
+  .back_left,.back_right{
+    width: 17%;
+    background: url("../assets/index-side1.png") no-repeat;
+    background-size: 100% 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    opacity: 0.7;
+  }
+
+  .back_right{
+    background-image: url("../assets/index-side2.png");
+    left: auto;
+    right: 0;
+  }
+</style>
